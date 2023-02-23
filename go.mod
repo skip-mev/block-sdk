0f7c95dbf716ca0d7c743ad42a0493641f624a29
Aleksandr Bezobchuk <alexanderbez@users.noreply.github.com>
2023-02-23 13:38:13 -0500

    chore: Setup Scaffolding for Protobuf (#1)

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/skip-mev/pob
 
 go 1.20
+
+require github.com/cosmos/gogoproto v1.4.4
---
+++ scripts/protocgen.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# How to run manually:
+# docker build --pull --rm -f "contrib/devtools/Dockerfile" -t cosmossdk-proto:latest "contrib/devtools"
+# docker run --rm -v $(pwd):/workspace --workdir /workspace cosmossdk-proto sh ./scripts/protocgen.sh
+
+set -e
+
+echo "Generating Protocol Buffer code..."
+cd proto
+proto_dirs=$(find ./pob -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+for dir in $proto_dirs; do
+  for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
+    if grep go_package $file &> /dev/null ; then
+      buf generate --template buf.gen.gogo.yaml $file
+    fi
+  done
+done
+
+cd ..
+
+# move proto files to the right places
+cp -r github.com/skip-mev/pob/* ./
+rm -rf github.com
+
+# go mod tidy --compat=1.20
