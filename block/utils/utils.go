b9d67617762c5159a531309f4b6ee7bdd548cfe3
David Terpay <35130517+davidterpay@users.noreply.github.com>
2023-09-28 11:10:13 -0400

    feat(ABCI): New Proposal Struct with Associated Metadata (#126)
    * new proto types for proposal info
    
    * new proposal type
    
    * nits
    
    * lane input
    
    * lint
    
    * feat(ABCI): Deprecating `CheckOrderHandler` with new Proposal MetaData (#127)
    
    * refactor without checkorder
    
    * nits
    
    * more nits
    
    * lint
    
    * nits
    
    * feat(ABCI): Updating MEV lane to have no `CheckOrder` handler + testing (#128)
    
    * updating mev lane
    
    * nits
    
    * preventing adding multiple bid txs in prepare
    
    * update

--- block/utils/utils.go
+++ block/utils/utils.go
@@ -5,23 +5,47 @@ import (
 	"encoding/hex"
 	"fmt"
 
-	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkmempool "github.com/cosmos/cosmos-sdk/types/mempool"
 )
 
-// GetTxHashStr returns the hex-encoded hash of the transaction alongside the
-// transaction bytes.
-func GetTxHashStr(txEncoder sdk.TxEncoder, tx sdk.Tx) ([]byte, string, error) {
+type (
+	// TxInfo contains the information required for a transaction to be
+	// included in a proposal.
+	TxInfo struct {
+		// Hash is the hex-encoded hash of the transaction.
+		Hash string
+		// Size is the size of the transaction in bytes.
+		Size int64
+		// GasLimit is the gas limit of the transaction.
+		GasLimit uint64
+		// TxBytes is the bytes of the transaction.
+		TxBytes []byte
+	}
+)
+
+// GetTxHashStr returns the TxInfo of a given transaction.
+func GetTxInfo(txEncoder sdk.TxEncoder, tx sdk.Tx) (TxInfo, error) {
 	txBz, err := txEncoder(tx)
 	if err != nil {
-		return nil, "", fmt.Errorf("failed to encode transaction: %w", err)
+		return TxInfo{}, fmt.Errorf("failed to encode transaction: %w", err)
 	}
 
 	txHash := sha256.Sum256(txBz)
 	txHashStr := hex.EncodeToString(txHash[:])
 
-	return txBz, txHashStr, nil
+	// TODO: Add an adapter to lanes so that this can be flexible to support EVM, etc.
+	gasTx, ok := tx.(sdk.FeeTx)
+	if !ok {
+		return TxInfo{}, fmt.Errorf("failed to cast transaction to GasTx")
+	}
+
+	return TxInfo{
+		Hash:     txHashStr,
+		Size:     int64(len(txBz)),
+		GasLimit: gasTx.GetGas(),
+		TxBytes:  txBz,
+	}, nil
 }
 
 // GetDecodedTxs returns the decoded transactions from the given bytes.
@@ -39,6 +63,21 @@ func GetDecodedTxs(txDecoder sdk.TxDecoder, txs [][]byte) ([]sdk.Tx, error) {
 	return decodedTxs, nil
 }
 
+// GetEncodedTxs returns the encoded transactions from the given bytes.
+func GetEncodedTxs(txEncoder sdk.TxEncoder, txs []sdk.Tx) ([][]byte, error) {
+	var encodedTxs [][]byte
+	for _, tx := range txs {
+		txBz, err := txEncoder(tx)
+		if err != nil {
+			return nil, fmt.Errorf("failed to encode transaction: %w", err)
+		}
+
+		encodedTxs = append(encodedTxs, txBz)
+	}
+
+	return encodedTxs, nil
+}
+
 // RemoveTxsFromLane removes the transactions from the given lane's mempool.
 func RemoveTxsFromLane(txs []sdk.Tx, mempool sdkmempool.Mempool) error {
 	for _, tx := range txs {
@@ -48,24 +87,4 @@ func RemoveTxsFromLane(txs []sdk.Tx, mempool sdkmempool.Mempool) error {
 	}
 
 	return nil
-}
-
-// GetMaxTxBytesForLane returns the maximum number of bytes that can be included in the proposal
-// for the given lane.
-func GetMaxTxBytesForLane(maxTxBytes, totalTxBytes int64, ratio math.LegacyDec) int64 {
-	// In the case where the ratio is zero, we return the max tx bytes remaining. Note, the only
-	// lane that should have a ratio of zero is the default lane. This means the default lane
-	// will have no limit on the number of transactions it can include in a block and is only
-	// limited by the maxTxBytes included in the PrepareProposalRequest.
-	if ratio.IsZero() {
-		remainder := maxTxBytes - totalTxBytes
-		if remainder < 0 {
-			return 0
-		}
-
-		return remainder
-	}
-
-	// Otherwise, we calculate the max tx bytes for the lane based on the ratio.
-	return ratio.MulInt64(maxTxBytes).TruncateInt().Int64()
 }
