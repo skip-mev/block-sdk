99dc7d46d0abc2b429015f42eecd156fc258776a
David Terpay <35130517+davidterpay@users.noreply.github.com>
2023-10-17 16:50:28 -0400

    fix(compare): Adding Sequence Number check on Compare Priority (#159)
    * adding seq num check on compare
    
    * nit
    
    * adding debug logging
    
    (cherry picked from commit aff0e228a3adbae1a634ec47d92ebc69b3d9e57c)

--- abci/abci.go
+++ abci/abci.go
@@ -75,7 +75,7 @@ func (h *ProposalHandler) PrepareProposalHandler() sdk.PrepareProposalHandler {
 		)
 
 		// Fill the proposal with transactions from each lane.
-		finalProposal, err := h.prepareLanesHandler(ctx, proposals.NewProposalWithContext(ctx, h.txEncoder))
+		finalProposal, err := h.prepareLanesHandler(ctx, proposals.NewProposalWithContext(h.logger, ctx, h.txEncoder))
 		if err != nil {
 			h.logger.Error("failed to prepare proposal", "err", err)
 			return &abci.ResponsePrepareProposal{Txs: make([][]byte, 0)}, err
@@ -141,7 +141,7 @@ func (h *ProposalHandler) ProcessProposalHandler() sdk.ProcessProposalHandler {
 
 		// Build handler that will verify the partial proposals according to each lane's verification logic.
 		processLanesHandler := ChainProcessLanes(partialProposals, h.mempool.Registry())
-		finalProposal, err := processLanesHandler(ctx, proposals.NewProposalWithContext(ctx, h.txEncoder))
+		finalProposal, err := processLanesHandler(ctx, proposals.NewProposalWithContext(h.logger, ctx, h.txEncoder))
 		if err != nil {
 			h.logger.Error("failed to validate the proposal", "err", err)
 			return &abci.ResponseProcessProposal{Status: abci.ResponseProcessProposal_REJECT}, err
