0fadd8cec318279d11a32ffe199da59d08c59885
aljo242 <alex@skip.money>
2023-11-02 12:59:44 -0400

    remove cmd

--- tests/app/testappd/cmd/root.go
+++ tests/app/testappd/cmd/root.go
@@ -279,7 +279,6 @@ func txCommand() *cobra.Command {
 		authcmd.GetBroadcastCommand(),
 		authcmd.GetEncodeCommand(),
 		authcmd.GetDecodeCommand(),
-		// authcmd.GetAuxToFeeCommand(),
 	)
 
 	return cmd
