4e57d96e4e4e00b60846e132c286a0d9bbedc040
aljo242 <alex@skip.money>
2023-11-02 11:56:32 -0400

    lint fix

--- tests/app/testappd/cmd/root.go
+++ tests/app/testappd/cmd/root.go
@@ -279,7 +279,7 @@ func txCommand() *cobra.Command {
 		authcmd.GetBroadcastCommand(),
 		authcmd.GetEncodeCommand(),
 		authcmd.GetDecodeCommand(),
-		authcmd.GetAuxToFeeCommand(),
+		// authcmd.GetAuxToFeeCommand(),
 	)
 
 	return cmd
