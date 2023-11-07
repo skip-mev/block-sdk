0deddf3ca939f30a7cac633f4662ad6435ca1234
aljo242 <alex@skip.money>
2023-11-07 14:15:31 -0500

    simplify

--- tests/integration/block_sdk_integration_test.go
+++ tests/integration/block_sdk_integration_test.go
@@ -6,12 +6,6 @@ import (
 
 	"cosmossdk.io/math"
 
-	"github.com/skip-mev/block-sdk/lanes/base"
-	"github.com/skip-mev/block-sdk/lanes/free"
-	"github.com/skip-mev/block-sdk/lanes/mev"
-
-	blocksdkmoduletypes "github.com/skip-mev/block-sdk/x/blocksdk/types"
-
 	testutil "github.com/cosmos/cosmos-sdk/types/module/testutil"
 	interchaintest "github.com/strangelove-ventures/interchaintest/v7"
 	"github.com/strangelove-ventures/interchaintest/v7/chain/cosmos"
@@ -19,8 +13,12 @@ import (
 	ictestutil "github.com/strangelove-ventures/interchaintest/v7/testutil"
 	"github.com/stretchr/testify/suite"
 
+	"github.com/skip-mev/block-sdk/lanes/base"
+	"github.com/skip-mev/block-sdk/lanes/free"
+	"github.com/skip-mev/block-sdk/lanes/mev"
 	"github.com/skip-mev/block-sdk/tests/integration"
 	auctiontypes "github.com/skip-mev/block-sdk/x/auction/types"
+	blocksdkmoduletypes "github.com/skip-mev/block-sdk/x/blocksdk/types"
 )
 
 var (
