c2fa9f2d68b881fa5a7ede342e0f37ae80017aa5
aljo242 <alex@skip.money>
2023-12-12 12:58:14 -0500

    basic setup

--- tests/integration/network_test.go
+++ tests/integration/network_test.go
@@ -2,7 +2,6 @@ package integration_test
 
 import (
 	"fmt"
-
 	"testing"
 
 	tmcli "github.com/cometbft/cometbft/libs/cli"
@@ -12,8 +11,10 @@ import (
 	"google.golang.org/grpc/status"
 
 	"github.com/skip-mev/block-sdk/testutils/networksuite"
-	"github.com/skip-mev/block-sdk/x/blocksdk/client/cli"
-	"github.com/skip-mev/block-sdk/x/blocksdk/types"
+	auctioncli "github.com/skip-mev/block-sdk/x/auction/client/cli"
+	auctiontypes "github.com/skip-mev/block-sdk/x/auction/types"
+	blocksdkcli "github.com/skip-mev/block-sdk/x/blocksdk/client/cli"
+	blocksdktypes "github.com/skip-mev/block-sdk/x/blocksdk/types"
 )
 
 // NetworkTestSuite is a test suite for network integration tests.
@@ -39,7 +40,7 @@ func (s *NetworkTestSuite) TestGetLanes() {
 
 		args []string
 		err  error
-		obj  []types.Lane
+		obj  []blocksdktypes.Lane
 	}{
 		{
 			name: "should return default lanes",
@@ -49,17 +50,56 @@ func (s *NetworkTestSuite) TestGetLanes() {
 	} {
 		s.T().Run(tc.name, func(t *testing.T) {
 			tc := tc
-			out, err := clitestutil.ExecTestCLICmd(val.ClientCtx, cli.CmdQueryLanes(), tc.args)
+			out, err := clitestutil.ExecTestCLICmd(val.ClientCtx, blocksdkcli.CmdQueryLanes(), tc.args)
 			if tc.err != nil {
 				stat, ok := status.FromError(tc.err)
 				require.True(t, ok)
 				require.ErrorIs(t, stat.Err(), tc.err)
 			} else {
 				require.NoError(t, err)
-				var resp types.QueryLanesResponse
+				var resp blocksdktypes.QueryLanesResponse
 				require.NoError(t, s.Network.Config.Codec.UnmarshalJSON(out.Bytes(), &resp))
 				require.NotNil(t, resp.Lanes)
 				require.ElementsMatch(t, tc.obj, resp.Lanes)
+			}
+		})
+	}
+}
+
+func (s *NetworkTestSuite) TestGetAuctionParams() {
+	s.T().Parallel()
+
+	val := s.Network.Validators[0]
+
+	common := []string{
+		fmt.Sprintf("--%s=json", tmcli.OutputFlag),
+	}
+	for _, tc := range []struct {
+		name string
+
+		args []string
+		err  error
+		obj  auctiontypes.Params
+	}{
+		{
+			name: "should return default params",
+			args: common,
+			obj:  auctiontypes.DefaultParams(),
+		},
+	} {
+		s.T().Run(tc.name, func(t *testing.T) {
+			tc := tc
+			out, err := clitestutil.ExecTestCLICmd(val.ClientCtx, auctioncli.CmdQueryParams(), tc.args)
+			if tc.err != nil {
+				stat, ok := status.FromError(tc.err)
+				require.True(t, ok)
+				require.ErrorIs(t, stat.Err(), tc.err)
+			} else {
+				require.NoError(t, err)
+				var resp auctiontypes.QueryParamsResponse
+				require.NoError(t, s.Network.Config.Codec.UnmarshalJSON(out.Bytes(), &resp.Params))
+				require.NotNil(t, resp)
+				require.Equal(t, tc.obj, resp.Params)
 			}
 		})
 	}
