a320e4e4cc574ea2f4faf8ccf7f609bddff9dd54
aljo242 <alex@skip.money>
2024-01-04 10:53:22 -0500

    per lane

--- abci/precommit/precommit.go
+++ abci/precommit/precommit.go
@@ -39,31 +39,58 @@ func NewMempoolEvictionPreCommiter(
 	}
 }
 
-// PreCommit returns a PreCommit handler that wraps the default PrecCommit handler and evicts invalid txs from the
+// PreCommit returns a PreCommit handler that wraps the default PreCommit handler and evicts invalid txs from the
 // app-side mempool
 func (m *MempoolEvictionPreCommiter) PreCommit() sdk.Precommiter {
 	return func(ctx sdk.Context) {
+		defer func() {
+			if rec := recover(); rec != nil {
+				m.logger.Error(
+					"panic in precommiter",
+					"err", rec)
+			}
+		}()
+
 		// call precommit per module
 		err := m.mm.Precommit(ctx)
 		if err != nil {
 			panic(err)
 		}
 
+		lanes, err := m.mempl.Registry(ctx)
+		if err != nil {
+			panic(err)
+		}
+
 		// add eviction hook
-		for iterator := m.mempl.Select(ctx, nil); iterator != nil; iterator = iterator.Next() {
-			tx := iterator.Tx()
-			_, err := m.anteHandler(ctx, tx, false)
-			if err != nil {
-				m.logger.Debug("error running antehandler on tx in precommit: removing from mempool",
-					"tx", tx,
-					"error", err,
-				)
-				err := m.mempl.Remove(tx)
+		for _, lane := range lanes {
+			for iterator := lane.Select(ctx, nil); iterator != nil; iterator = iterator.Next() {
+				tx := iterator.Tx()
+
+				// run ante handler on all tx
+				_, err := m.anteHandler(ctx, tx, false)
 				if err != nil {
-					panic(err)
+					m.logger.Debug("error running antehandler on tx in precommit: removing from mempool",
+						"tx", tx,
+						"error", err,
+					)
+					err := m.mempl.Remove(tx)
+					if err != nil {
+						panic(err)
+					}
+				}
+
+				// check if tx matches still
+				if !lane.Match(ctx, tx) {
+					m.logger.Debug("tx does not match lane: removing from mempool",
+						"tx", tx,
+					)
+					err := m.mempl.Remove(tx)
+					if err != nil {
+						panic(err)
+					}
 				}
 			}
-
 		}
 	}
 }
